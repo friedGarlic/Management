667cd9b5c68348fa6f82581b1e5e67749b00c1be
remcarlmerza <remcarlmerza@gmail.com>
2023-09-24 19:58:32 +0800

    added update command handler in leave request[features]

---
+++ ManagementApp/Features/LeaveRequest/Handler/Command/UpdateLeaveRequest_CommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Management.Application.Features.LeaveRequest.Request.Command;
+using ManagementApp.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Application.Features.LeaveRequest.Handler.Command
+{
+    internal class UpdateLeaveRequest_CommandHandler : IRequestHandler<UpdateLeaveRequest_CommandRequest, Unit>
+    {
+        private readonly ILeaveRequestRepository _repository;
+        private readonly Mapper _mapper;
+
+        public UpdateLeaveRequest_CommandHandler(ILeaveRequestRepository repository, Mapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveRequest_CommandRequest commandRequest, CancellationToken token)
+        {
+            //get id
+            var leaveRequest = await _repository.Get(commandRequest.LeaveRequestDTO.Id);
+
+            //map
+            _mapper.Map(commandRequest.LeaveRequestDTO, leaveRequest);
+
+            //update
+            await _repository.Update(leaveRequest);
+
+            //return value
+            return Unit.Value;
+        }
+    }
+}
---
+++ ManagementApp/Features/LeaveRequest/Request/Command/UpdateLeaveRequest_CommandRequest.cs
@@ -0,0 +1,15 @@
+﻿using Management.Application.DTOs.LeaveRequest;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Application.Features.LeaveRequest.Request.Command
+{
+    internal class UpdateLeaveRequest_CommandRequest : IRequest<Unit>
+    {
+        public LeaveRequestDTO LeaveRequestDTO { get; set; }
+    }
+}
