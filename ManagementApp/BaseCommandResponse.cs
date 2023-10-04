026324828bc4aab31af721d1b36b7b29ccca561f
remcarlmerza <remcarlmerza@gmail.com>
2023-10-04 16:57:59 +0800

    create basecommandResponse class

---
+++ ManagementApp/BaseCommandResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Application
+{
+    public class BaseCommandResponse
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; }
+
+        public bool Success { get; set; }
+
+        public List<string> Errors { get; set; }
+    }
+}
