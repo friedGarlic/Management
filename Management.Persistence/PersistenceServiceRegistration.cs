486225a35870608e99e1b755040fafcd2fc9dbda
remcarlmerza <remcarlmerza@gmail.com>
2023-10-09 04:43:16 +0800

    added services registration, add scope for datatyperepository, leaveallocation, leave request repos.

--- Management.Persistence/PersistenceServiceRegistration.cs
+++ Management.Persistence/PersistenceServiceRegistration.cs
@@ -1,24 +1,25 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Management.Persistence.Repositories;
+using ManagementApp.Contracts;
 
 namespace Management.Persistence
 {
-    public class PersistenceServiceRegistration
+    public static class PersistenceServiceRegistration
     {
-        public PersistenceServiceRegistration()
-        {
-            
-        }
-
-        public static IServiceCollection ConfigurePersistenceServices(IServiceCollection services, 
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, 
             IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            services.AddDbContext<ManagementdbContext>(option => option.UseSqlServer(configuration.GetConnectionString("ManagementConnectionString")));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            services.AddScoped<IDataTypeRepository,DataTypeRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+
+            return services;
         }
     }
 }
