a98ac2e7ef7c6d36a48a7d24eca59d2bbb4c3a7b
remcarlmerza <remcarlmerza@gmail.com>
2023-10-23 17:36:13 +0800

    added migration for seeding data type

--- Management.Persistence/Migrations/20231023033756_SeedDataType.Designer.cs
+++ Management.Persistence/Migrations/20231023033756_SeedDataType.Designer.cs
@@ -4,6 +4,7 @@ using Management.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Management.Persistence.Migrations
 {
     [DbContext(typeof(ManagementdbContext))]
-    partial class ManagementdbContextModelSnapshot : ModelSnapshot
+    [Migration("20231023033756_SeedDataType")]
+    partial class SeedDataType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Management.Persistence/Migrations/20231023033756_SeedDataType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Management.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DbDataType",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DateModified", "ModifiedBy", "Name" },
+                values: new object[,]
+                {
+                    { 1, "", new DateTime(2023, 10, 23, 11, 37, 55, 918, DateTimeKind.Local).AddTicks(7773), new DateTime(2023, 10, 23, 11, 37, 55, 918, DateTimeKind.Local).AddTicks(7783), "", "SickLeave" },
+                    { 2, "", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 10, 23, 11, 37, 55, 918, DateTimeKind.Local).AddTicks(7785), "", "Vacation" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DbDataType",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DbDataType",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
